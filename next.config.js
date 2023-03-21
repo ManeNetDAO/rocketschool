5c0b6934ecbd9a18fbfa2063f54fc7f40d43001a
Isaac Ibiapina <isaac1@mail.usf.edu>
2023-03-21 15:50:36 -0400

    initial commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV !== 'production'
+})
+
+module.exports = withPWA()
