3b81133e4ca528b59ce2aa414dec99524a84783d
Arbora <noah@unstoppablegames.io>
2023-11-08 14:06:00 -0500

    Update videos

--- src/content/runYourOwnNode.ts
+++ src/content/runYourOwnNode.ts
@@ -14,17 +14,37 @@ export const videos: Array<ElemData> = [
     about: 'Set up your hardware and operating system',
     url: 'https://www.youtube.com/embed/bQK9Yr1BpQw',
   },
+  // 2.3 Securing your Node
   {
-    title: '2.3 Securing your Node',
+    title: '2.3.1 Harden SSH',
     about:
-      'Set up SSH, disable password login, set up a firewall, and consider several more advanced security options',
-    url: 'https://www.youtube.com/embed/bQK9Yr1BpQw',
+      'Disable insecure ways of accessing your node over SSH',
+    url: 'https://www.youtube.com/embed/H6kOq-wW1Pk',
   },
   {
-    title: '2.4 Installing Rocket Pool',
+    title: '2.3.2 Enabling a Firewall',
     about:
-      'Install Docker, download the Rocket Pool CLI, and install the Smartnode Stack',
-    url: 'https://www.youtube.com/embed/bQK9Yr1BpQw',
+      'Set up and enable a firewall to prevent unwanted contact with your node',
+    url: 'https://www.youtube.com/embed/ng5lAKlNfnk',
+  },
+  {
+    title: '2.3.3 Automatic Security Updates',
+    about:
+      'Set up and enable automatic security updates for your node',
+    url: 'https://www.youtube.com/embed/h7LIG3dFNZM',
+  },
+  // 2.4 Installing Rocket Pool
+  {
+    title: '2.4.1 Installing Rocket Pool',
+    about:
+      'Download the Rocket Pool CLI, and install the Smartnode Stack',
+    url: 'https://www.youtube.com/embed/320ICgDAQCk',
+  },
+  {
+    title: '2.4.2 Install Smartnode Stack',
+    about:
+      'Install the Rocket Pool Smartnode Stack',
+    url: 'https://www.youtube.com/embed/HRKU-GQHQkg',
   },
   {
     title: '2.5 Deciding on Testnet',
@@ -36,24 +56,24 @@ export const videos: Array<ElemData> = [
     title: '2.6 Configuring via the Wizard',
     about:
       'Configure a network, choose execution and consensus clients, and configure a fallback node and metrics',
-    url: 'https://www.youtube.com/embed/bQK9Yr1BpQw',
+    url: 'https://www.youtube.com/embed/VZxIU8EXUH4',
   },
   {
     title: '2.7 Starting Rocket Pool',
     about:
       'Start and stop the Rocket Pool service, check the version, and check sync status',
-    url: 'https://www.youtube.com/embed/bQK9Yr1BpQw',
+    url: 'https://www.youtube.com/embed/4SFNuOBHbQE',
   },
   {
     title: '2.8 Creating the Node Wallet',
     about: 'Create your node wallet',
-    url: 'https://www.youtube.com/embed/bQK9Yr1BpQw',
+    url: 'https://www.youtube.com/embed/MljMbQ9TIik',
   },
   {
     title: '2.9 Preparing your Node for Operation',
     about:
       'Load your node wallet, register your node, and set your withdrawal address',
-    url: 'https://www.youtube.com/embed/bQK9Yr1BpQw',
+    url: 'https://www.youtube.com/embed/WjUCuV9G5yg',
   },
   {
     title: '2.10 Opting into the Smoothing Pool and MEV',
@@ -64,6 +84,6 @@ export const videos: Array<ElemData> = [
     title: '2.11 Creating a Minipool',
     about:
       'Take the final step - stake your RPL and ETH and create your Rocket Pool minipool',
-    url: 'https://www.youtube.com/embed/bQK9Yr1BpQw',
+    url: 'https://www.youtube.com/embed/tXxSqKw0RvU',
   },
 ];
