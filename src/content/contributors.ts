4a7f0671d7af9dabd7a87c60f4fa6fa4e1b81273
Arbora <noah@unstoppablegames.io>
2024-03-08 14:54:24 -0500

    Add Austonst and update Doubtstars contributor profiles.

--- src/content/contributors.ts
+++ src/content/contributors.ts
@@ -7,7 +7,7 @@ export const contributors = [
   },
   {
     name: 'Doubtstars',
-    text: '',
+    text: 'DoubtStars is a Learning and Development professional and instructional designer who has worked with many large tech companies in the past, and has a long history of involvement in the web3 community, project managing Rocket School and planning HodlerCon, the EthFinance community’s biannual meetup. She is the Co-Founder TokenomicsExplained.com with her husband LogristheBard.',
     image: 'doubtstars.webp',
     link: 'https://tokenomicsexplained.com/',
   },
@@ -28,6 +28,12 @@ export const contributors = [
     image: 'eyezick.png',
     text: '',
     link: 'https://github.com/eyezick',
+  },
+  {
+    name: 'Austonst',
+    image: 'austonst.webp',
+    text: 'Auston is a long-time member of the Ethereum community, and frequently contributes to the EthFinance and EthStaker communities. He is part of the Aestus Relay team, operates solo and Rocket Pool validators, and really likes the phrase "credible neutrality".',
+    link: 'https://etherscan.io/name-lookup-search?id=austonst.eth',
   },
   {
     name: 'ShfRyn',
